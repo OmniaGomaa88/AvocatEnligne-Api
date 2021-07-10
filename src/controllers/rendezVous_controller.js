99ed50461d8b0c98e9141b6797f02009762e1028
OmniaGomaa88 <minna1172018@gmail.com>
2021-07-10 14:46:08 +0200

    add redezVous manque client ID

--- src/controllers/rendezVous_controller.js
+++ src/controllers/rendezVous_controller.js
@@ -1,32 +1,31 @@
 const RendezVous = require("../models/rendez_vous");
 const { request, response } = require("express");
 const { OK, SERVER_ERROR } = require("../helpers/stuts_code");
-
-exports.newRendezVous = (request, response) => {
-  const avocatId = request.params.id;
-
-  const {
-    client_situation,
-    date,
+// 
+ exports.newRendezVous = (request, response) => {
+   const avocatId = request.params.id;
+ 
+   const {
+     client_situation,
+     date,
     // pour l'instant on vais avoire le  client id manuellement
-    clientId,
-  } = request.body;
-  RendezVous.addRendezVous(avocatId, request.body, (error, result) => {
-    if (error) {
-      console.log(error);
-      response.status(SERVER_ERROR).json({
-        message: error,
-      });
-    } else {
-      response.status(OK).json({
-        id: result[0].id,
-        client_situation: request.body.client_situation,
-        date: request.body.date,
-        heure: request.body.heure,
-      });
-    }
-  });
-};
+     clientId,
+   } = request.body;
+   RendezVous.addRendezVous(avocatId, request.body, (error, result) => {
+     if (error) {
+       console.log(error);
+       response.status(SERVER_ERROR).json({
+         message: error,
+       });
+     } else {
+       response.status(OK).json({
+         client_situation: request.body.client_situation,
+         date: request.body.date,
+         heure: request.body.heure
+       });
+     }
+   });
+ };
 // get avocat rendez-vous  avec client data 
 exports.findMesRendezVous=(request,response)=>{
  // pour l'instant on vais avoire avocat  id manuellement
@@ -40,7 +39,7 @@ exports.findMesRendezVous=(request,response)=>{
     } else {
       response.status(OK).json({
         id: result[0].id,
-        clint:{
+        client:{
         prenom:result[0].prénom,
         nom:result[0].Nom,
         email:result[0].Email,
--- src/models/rendez_vous.js
+++ src/models/rendez_vous.js
@@ -1,23 +1,23 @@
 const db = require("../db/db");
-// exports.addRendezVous = async (avocatId, data, Callback) => {
-  // let addRendezVousdQuery = await `INSERT INTO RendezVous 
-    // (client_situation,date,heure,avocat_id,client_id) VALUES
-    // ("${data.client_situation}",
-    // "${data.date}",
-    // "${data.heure}",
-    // "${avocatId}",
-    // "${data.clientId}");
-    //  WHERE id=${avocatId}`;
-  // db.query(addRendezVousdQuery, (error, result) => {
-    // if (error) {
-      // Callback(error, null);
-      // console.log("error:", error);
-      // return;
-    // } else {
-      // Callback(null, result);
-    // }
-  // });
-// };
+ exports.addRendezVous = async (avocatId, data, Callback) => {
+   let addRendezVousdQuery = await `INSERT INTO RendezVous 
+     (client_situation,date,heure,avocat_id,client_id) VALUES
+     ("${data.client_situation}",
+     "${data.date}",
+     "${data.heure}",
+     "${avocatId}",
+     "${data.clientId}");`;
+   db.query(addRendezVousdQuery, (error, result) => {
+     if (error) {
+      Callback(error, null);
+      console.log("error:", error);
+      return;
+    } else {
+      console.log(result)
+       Callback(null, result);
+    }
+ });
+};
 // get avocat rendez-vous  avec client data 
 exports.getMesRendezVous= async(avocatId,Callback)=>{
   let getRendezVousQuery = await `SELECT * FROM
